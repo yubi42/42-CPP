444ca8c196eab241a06766530a9d35be768facb4
yubi42 <undercoverbabe12@gmail.com>
2024-06-27 15:06:47 +0200

    del comments

--- CPP05/ex03/main.cpp
+++ CPP05/ex03/main.cpp
@@ -3,23 +3,6 @@
 #include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 
-/* int main ()
-{
-Intern someRandomIntern;
-Intern secondIntern(someRandomIntern);
-AForm *form;
-
-form = secondIntern.makeForm("robotomy request", "Bender");
-std::cout << *form << std::endl;
-delete form;
-std::cout << std::endl;
-
-form = secondIntern.makeForm("shrubbery creation", "shubby");
-std::cout << *form << std::endl;
-delete form;
-}
- */
-
 int main ()
 {
 Intern someRandomIntern;
