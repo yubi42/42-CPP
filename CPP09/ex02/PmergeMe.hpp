de7bc9c08926bbfc6e4a91dd89f64fb1214fd3c2
yubi42 <undercoverbabe12@gmail.com>
2024-09-13 13:53:41 +0200

    add other ways for johnson

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -16,51 +16,33 @@
 template<typename T>
 struct TypeName 
 {
-    static std::string name() 
-    {
-        return ("Unknown type");
-    }
+    static std::string name() { return ("Unknown type"); }
 };
 
 template<>
 struct TypeName<std::vector<int> > 
 {
-    static std::string name() 
-    {
-        return ("std::vector<int>");
-    }
+    static std::string name() { return ("std::vector<int>"); }
 };
 
 template<>
 struct TypeName<std::deque<int> > 
 {
-    static std::string name() 
-    {
-        return ("std::deque<int>");
-    }
+    static std::string name() { return ("std::deque<int>"); }
 };
 
 template<>
 struct TypeName<std::list<int> > 
 {
-    static std::string name() 
-    {
-        return ("std::list<int>");
-    }
+    static std::string name() { return ("std::list<int>"); }
 };
 
 
 template<typename T>
-void sortContainer(T &arr) 
-{
-    std::sort(arr.begin(), arr.end());
-}
+void sortContainer(T &arr) { std::sort(arr.begin(), arr.end()); }
 
 template<typename T>
-void sortContainer(std::list<T> &arr) 
-{
-    arr.sort();
-}
+void sortContainer(std::list<T> &arr) { arr.sort(); }
 
 class PmergeMe
 {
@@ -87,10 +69,7 @@ public:
     }
 
     template<typename T>
-    std::string getType(const T&) 
-    {
-        return TypeName<T>::name();
-    }
+    std::string getType(const T&) { return TypeName<T>::name(); }
 
     template<typename T>
     void displayResults(std::pair<clock_t, clock_t> time, const T &arr)
@@ -143,6 +122,76 @@ public:
             typename T::iterator insert_pos = std::upper_bound(arr.begin(), arr.end(), *big_it);
             arr.insert(insert_pos, *big_it);
         }
+    };
+
+    template<typename T>
+    T johnsonSort2(const T &arr)
+    {
+        T big_values_arr;
+        T small_values_arr;
+        typename T::const_iterator end = arr.end();
+        for(typename T::const_iterator it = arr.begin(); it != end; ++it) 
+        {
+            typename T::const_iterator next_it = it;
+            ++next_it;
+            if (next_it != end) 
+            {
+                if (*it > *next_it) 
+                {
+                    big_values_arr.push_back(*it);
+                    small_values_arr.push_back(*next_it);
+                } 
+                else 
+                {
+                    big_values_arr.push_back(*next_it);
+                    small_values_arr.push_back(*it);
+                }
+                ++it;
+            }
+            else
+                small_values_arr.push_back(*it);
+        }
+        sortContainer(small_values_arr);
+        for (typename T::iterator big_it = big_values_arr.begin(); big_it != big_values_arr.end(); ++big_it)
+        {
+            typename T::iterator insert_pos = std::upper_bound(small_values_arr.begin(), small_values_arr.end(), *big_it);
+            small_values_arr.insert(insert_pos, *big_it);
+        }
+        return(small_values_arr);
+    };
+
+    template<typename T>
+    T johnsonSort3(const T &arr)
+    {
+        T big_values_arr;
+        T small_values_arr;
+        size_t len = arr.size();
+        for(size_t i = 0; i < len; ++i) 
+        {
+            if (i + 1 < len) 
+            {
+                if (arr[i] > arr[i + 1]) 
+                {
+                    big_values_arr.push_back(arr[i]);
+                    small_values_arr.push_back(arr[i + 1]);
+                } 
+                else 
+                {
+                    big_values_arr.push_back(arr[i + 1]);
+                    small_values_arr.push_back(arr[i]);
+                }
+                ++i;
+            }
+            else
+                small_values_arr.push_back(arr[i]);
+        }
+        sortContainer(small_values_arr);
+        for (typename T::iterator big_it = big_values_arr.begin(); big_it != big_values_arr.end(); ++big_it)
+        {
+            typename T::iterator insert_pos = std::upper_bound(small_values_arr.begin(), small_values_arr.end(), *big_it);
+            small_values_arr.insert(insert_pos, *big_it);
+        }
+        return(small_values_arr);
     };
 };
 
--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -11,40 +11,96 @@ int main(int ac, char **av)
     PmergeMe pmerge;
     std::pair<clock_t, clock_t> stop_time;
 
-    std::vector<int> vec;
-    pmerge.parseInput(ac, av, vec);
-    pmerge.displayArr("Before:", vec);
-    stop_time.first = clock();
-    pmerge.johnsonSort(vec);
-    stop_time.second = clock();
-    pmerge.displayArr("After:", vec);
-    pmerge.displayResults(stop_time, vec);
+    {
+        std::vector<int> vec;
+        pmerge.parseInput(ac, av, vec);
+        pmerge.displayArr("Before:", vec);
+        stop_time.first = clock();
+        pmerge.johnsonSort(vec);
+        stop_time.second = clock();
+        pmerge.displayArr("After:", vec);
+        std::cout << "johnson 1: use original vector for small values, new one to store big values << less memory usage but slower" << std::endl;
+        std::cout << "johnson 2: 2 new vectors to store big and small values | using T::iterator" << std::endl;
+        std::cout << "johnson 3: 2 new vectors to store big and small values | using size_t i << works for random access containers like vector and deque but not for list, map, set..." << std::endl;
+        std::cout << "johnson 1: ";
+        pmerge.displayResults(stop_time, vec);
+    }
+    {
+        std::vector<int> vec;
+        pmerge.parseInput(ac, av, vec);
+        // pmerge.displayArr("Before:", vec);
+        stop_time.first = clock();
+        vec = pmerge.johnsonSort2(vec);
+        stop_time.second = clock();
+        // pmerge.displayArr("After:", vec);
+        std::cout << "johnson 2: ";
+        pmerge.displayResults(stop_time, vec);
+    }
+    {
+        std::vector<int> vec;
+        pmerge.parseInput(ac, av, vec);
+        // pmerge.displayArr("Before:", vec);
+        stop_time.first = clock();
+        vec = pmerge.johnsonSort3(vec);
+        stop_time.second = clock();
+        // pmerge.displayArr("After:", vec);
+        std::cout << "johnson 3: ";
+        pmerge.displayResults(stop_time, vec);
+    }
+    {
+        std::deque<int> deq;
+        pmerge.parseInput(ac, av, deq);
+        // pmerge.displayArr("Before:", deq);
+        stop_time.first = clock();
+        pmerge.johnsonSort(deq);
+        stop_time.second = clock();
+        // pmerge.displayArr("After:", deq);
+        std::cout << "johnson 1: ";
+        pmerge.displayResults(stop_time, deq);
+    }
+    {
+        std::deque<int> deq;
+        pmerge.parseInput(ac, av, deq);
+        // pmerge.displayArr("Before:", deq);
+        stop_time.first = clock();
+        deq = pmerge.johnsonSort2(deq);
+        stop_time.second = clock();
+        // pmerge.displayArr("After:", deq);
+        std::cout << "johnson 2: ";
+        pmerge.displayResults(stop_time, deq);
+    }
+    {
+        std::deque<int> deq;
+        pmerge.parseInput(ac, av, deq);
+        // pmerge.displayArr("Before:", deq);
+        stop_time.first = clock();
+        deq = pmerge.johnsonSort3(deq);
+        stop_time.second = clock();
+        // pmerge.displayArr("After:", deq);
+        std::cout << "johnson 3: ";
+        pmerge.displayResults(stop_time, deq);
+    }
+    {
+        std::list<int> lis;
+        pmerge.parseInput(ac, av, lis);
+        // pmerge.displayArr("Before:", lis);
+        stop_time.first = clock();
+        pmerge.johnsonSort(lis);
+        stop_time.second = clock();
+        // pmerge.displayArr("After:", lis);
+        std::cout << "johnson 1: ";
+        pmerge.displayResults(stop_time, lis);
+    }
+    {
+        std::list<int> lis;
+        pmerge.parseInput(ac, av, lis);
+        // pmerge.displayArr("Before:", lis);
+        stop_time.first = clock();
+        lis = pmerge.johnsonSort2(lis);
+        stop_time.second = clock();
+        // pmerge.displayArr("After:", lis);
+        std::cout << "johnson 2: ";
+        pmerge.displayResults(stop_time, lis);
+    }
 
-    std::deque<int> deq;
-    pmerge.parseInput(ac, av, deq);
-    // pmerge.displayArr("Before:", deq);
-    stop_time.first = clock();
-    pmerge.johnsonSort(deq);
-    stop_time.second = clock();
-    // pmerge.displayArr("After:", deq);
-    pmerge.displayResults(stop_time, deq);
-
-    std::list<int> lis;
-    pmerge.parseInput(ac, av, lis);
-    // pmerge.displayArr("Before:", lis);
-    stop_time.first = clock();
-    pmerge.johnsonSort(lis);
-    stop_time.second = clock();
-    // pmerge.displayArr("After:", lis);
-    pmerge.displayResults(stop_time, lis);
-
-/*     std::queue<int> que;
-    pmerge.parseInput(ac, av, que);
-    // pmerge.displayArr("Before:", que);
-    stop_time.first = clock();
-    pmerge.johnsonSort(que);
-    stop_time.second = clock();
-    // pmerge.displayArr("After:", que);
-    pmerge.displayResults(stop_time, que);
-    return (0); */
 }
